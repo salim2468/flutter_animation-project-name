1fe2a4d9920ed5e75c7e6ab2c17f16a3bd296e8a
salim2468 <salimmaharjan2468@gmail.com>
2023-09-03 17:25:32 +0545

    inital commit

---
+++ lib/constant/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color kBlack = Colors.black;
+  static const Color kWhite = Colors.white;
+  static Color kPrimary = Colors.grey.shade500;
+  static Color kSecondary = const Color(0xffff9900);
+  static Color kBackground = Colors.grey.shade200;
+}
---
+++ lib/constant/url.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+String uri = 'http://192.168.101.4:3000';
